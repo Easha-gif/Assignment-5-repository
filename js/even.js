e965e843f34e85cb938c8f683f30d8839e315534
Easha-gif <dreameasha@gmail.com>
2024-09-22 23:26:28 +0600

    first section done

---
+++ js/even.js
@@ -0,0 +1,8 @@
+// blog btn
+document.getElementById("blog-btn").addEventListener('click' , function () {
+   window.location.href="blog.html" 
+})
+// home btn 
+document.getElementById("home-btn").addEventListener('click' , function(){
+    window.location.href="index.html"
+})
